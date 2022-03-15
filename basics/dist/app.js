d219bbdf9a357e03360c35e14f6c59e68078abce
pimoux <luka.vegeta@laposte.net>
2022-03-15 15:25:14 +0100

    added infinite scroll posts

--- basics/dist/app.js
+++ basics/dist/app.js
@@ -1,2 +1,58 @@
 "use strict";
-console.log("test");
+const postsContainerElt = document.querySelector('#posts');
+const fetchButtonElt = document.querySelector("#fetch-button");
+const noRemainingPostsElt = document.querySelector("#no-remaining-posts");
+const usernames = ["Pimoux", "Auraah", "Eliminate", "Swarmsii", "Lothus", "DaZeRo", "AMDuskia", "Hikanari", "Cribob", "Lochie"];
+let data = [];
+let showingData = [];
+let postsDisplayed = 0;
+let isPostsFetched = false;
+const fetchPosts = () => {
+    return fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(res => res.json());
+};
+const updatePosts = (data) => {
+    data.forEach(obj => {
+        let div = document.createElement('div');
+        div.classList.add("post");
+        let titleElement = document.createElement('h2');
+        let bodyParagraph = document.createElement('p');
+        let spanElt = document.createElement("span");
+        let title = document.createTextNode(obj.title);
+        let body = document.createTextNode(obj.body);
+        let spanText = document.createTextNode(`written by ${usernames[obj.userId - 1]}`);
+        spanElt.appendChild(spanText);
+        titleElement.appendChild(title);
+        bodyParagraph.appendChild(body);
+        div.appendChild(titleElement);
+        div.appendChild(bodyParagraph);
+        div.appendChild(spanElt);
+        postsContainerElt.appendChild(div);
+    });
+};
+const concatPosts = () => {
+    if (postsDisplayed < 100) {
+        const nextPosts = data.slice(postsDisplayed, postsDisplayed + 10);
+        postsDisplayed += 10;
+        updatePosts(nextPosts);
+    }
+    else {
+        noRemainingPostsElt.classList.remove("hidden");
+        noRemainingPostsElt.classList.add("inline");
+    }
+};
+fetchButtonElt.addEventListener("click", () => {
+    fetchPosts().then(res => {
+        isPostsFetched = true;
+        postsDisplayed += 10;
+        data = res;
+        showingData = res.slice(0, postsDisplayed);
+        updatePosts(showingData);
+    });
+});
+window.addEventListener("scroll", () => {
+    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+    if (isPostsFetched && scrollTop + clientHeight === scrollHeight) {
+        concatPosts();
+    }
+});
--- basics/src/app.ts
+++ basics/src/app.ts
@@ -1,1 +1,61 @@
-console.log("test");
+const postsContainerElt = <HTMLDivElement>document.querySelector('#posts');
+const fetchButtonElt = <HTMLButtonElement>document.querySelector("#fetch-button");
+const noRemainingPostsElt = <HTMLParagraphElement>document.querySelector("#no-remaining-posts");
+const usernames: string[] = ["Pimoux", "Auraah", "Eliminate", "Swarmsii", "Lothus", "DaZeRo", "AMDuskia", "Hikanari", "Cribob", "Lochie"];
+let data: {userId: number, id: number, title: string, body: string}[] = [];
+let showingData: {userId: number, id: number, title: string, body: string}[] = [];
+let postsDisplayed: number = 0;
+let isPostsFetched: boolean = false;
+
+const fetchPosts = () =>  {
+    return fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+}
+
+const updatePosts = (data: {userId: number, id: number, title: string, body: string}[]) => {
+    data.forEach(obj => {
+        let div = <HTMLDivElement>document.createElement('div');
+        div.classList.add("post");
+        let titleElement = <HTMLHeadingElement>document.createElement('h2');
+        let bodyParagraph = <HTMLParagraphElement>document.createElement('p');
+        let spanElt = <HTMLSpanElement>document.createElement("span");
+        let title = <Text>document.createTextNode(obj.title);
+        let body = <Text>document.createTextNode(obj.body);
+        let spanText = <Text>document.createTextNode(`written by ${usernames[obj.userId - 1]}`)
+        spanElt.appendChild(spanText);
+        titleElement.appendChild(title);
+        bodyParagraph.appendChild(body);
+        div.appendChild(titleElement);
+        div.appendChild(bodyParagraph);
+        div.appendChild(spanElt);
+        postsContainerElt.appendChild(div);
+    })
+}
+
+const concatPosts = () => {
+    if (postsDisplayed < 100) {
+        const nextPosts = data.slice(postsDisplayed, postsDisplayed + 10);
+        postsDisplayed += 10;
+        updatePosts(nextPosts);
+    } else {
+        noRemainingPostsElt.classList.remove("hidden");
+        noRemainingPostsElt.classList.add("inline");
+    }
+}
+
+fetchButtonElt.addEventListener("click", () => {
+    fetchPosts().then(res => {
+        isPostsFetched = true;
+        postsDisplayed += 10;
+        data = res;
+        showingData = res.slice(0, postsDisplayed);
+        updatePosts(showingData);
+    });
+});
+
+window.addEventListener("scroll", () => {
+    const {scrollTop, scrollHeight, clientHeight} = document.documentElement;
+    if(isPostsFetched && scrollTop + clientHeight === scrollHeight) {
+        concatPosts();
+    }
+})
